d8ef7259765e7c7bd87c765069738e42c667d09f
uhyo <uhyo@uhy.ooo>
2018-03-15 23:49:57 +0900

    fix error of production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,63 @@
 "use strict";
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
-module.exports={
-    mode: production ? 'production' : 'development',
-    devtool: production ? undefined : 'source-map',
-    entry: './src/entrypoint.tsx',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'source-map-loader',
-                enforce: 'pre',
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules|\.d\.ts$/,
-                loader: 'awesome-typescript-loader',
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader?modules&camelCase', 'postcss-loader'],
-            },
-            {
-                test: /\.html$/,
-                loaders: ['ignore-loader', 'file-loader?name=[name].[ext]'],
-            },
-            {
-                test: /\.(?:png|gif)$/,
-                loaders: ['url-loader', 'img-loader'],
-            }
+module.exports = {
+  mode: production ? "production" : "development",
+  devtool: production ? undefined : "source-map",
+  entry: "./src/entrypoint.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "source-map-loader",
+        enforce: "pre"
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules|\.d\.ts$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style-loader",
+          "css-loader?modules&camelCase",
+          "postcss-loader"
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        // こうしないとなぜかReactがこわれる
-        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-    },
-    performance: {
-        //bye bye, FIXME...
-        hints: false,
-    },
-    
-    devServer: {
-        contentBase: './dist',
-        port: 8080,
-    }
+      },
+      {
+        test: /\.html$/,
+        loaders: ["ignore-loader", "file-loader?name=[name].[ext]"]
+      },
+      {
+        test: /\.(?:png|gif)$/,
+        loaders: ["url-loader", "img-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    // こうしないとなぜかReactがこわれる
+    modules: [path.resolve(__dirname, "node_modules"), "node_modules"]
+  },
+  performance: {
+    //bye bye, FIXME...
+    hints: false
+  },
+
+  devServer: {
+    contentBase: "./dist",
+    port: 8080
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
 };
